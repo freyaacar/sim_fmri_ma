756c5f6b76db260b730b9d7957fc317892573046
facar <freya.acar@gmail.com>
2017-04-12 12:35:24 +0200

    Read in t-maps

--- sim_ma.R
+++ sim_ma.R
@@ -4,9 +4,23 @@
   # This consists of 21 studies, the first 10 are analysed with SPM, the last 11
   # with FSL
 ################################################################################
-  # A pain meta-analysis from NeuroVault is used (http://neurovault.org/collections/1425/)
-  # This consists of 21 studies, the first 10 are analysed with SPM, the last 11
-  # with FSL
+  # Preamble
+    nstud <- 21
+    n.form <- formatC(c(1:nstud), width=2, flag="0")
+    DIM <- c(91,109,91)
+    library(oro.nifti)
+
+  # read in t-maps
+    tmaps <- array(data=NA,dim=c(nstud,DIM))
+    for (n in 1:nstud) {
+    	if (n < 11){
+    		tmaps[n,,,]<-readNIfTI(paste("21painstudies(NIDM-Results)/pain_",n.form[n],".nidm/TStatistic.nii.gz",sep=""), verbose = FALSE, warn = -1, reorient = TRUE, call = NULL)
+    	}
+    	else {
+    		tmaps[n,,,]<-readNIfTI(paste("21painstudies(NIDM-Results)/pain_",n.form[n],".nidm/TStatistic_T001.nii.gz",sep=""), verbose = FALSE, warn = -1, reorient = TRUE, call = NULL)
+    	}
+    }
+
 
   # tmaps
 
