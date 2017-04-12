2942754cc3f06999ad4f89e1818e15c020683aa2
facar <freya.acar@gmail.com>
2017-04-12 11:36:13 +0200

    Add structure to the file

--- sim_ma.R
+++ sim_ma.R
@@ -1,1 +1,69 @@
-home <- ('')
+################################################################################
+# Read in dataset
+  # A pain meta-analysis from NeuroVault is used (http://neurovault.org/collections/1425/)
+  # This consists of 21 studies, the first 10 are analysed with SPM, the last 11
+  # with FSL
+################################################################################
+  # A pain meta-analysis from NeuroVault is used (http://neurovault.org/collections/1425/)
+  # This consists of 21 studies, the first 10 are analysed with SPM, the last 11
+  # with FSL
+
+  # tmaps
+
+  # z-maps
+
+  # ES-maps
+
+################################################################################
+# Compute within- and between study variance
+################################################################################
+  # within-study variance
+
+  # between-study variance
+
+
+################################################################################
+# Determine parameters for the simulations
+################################################################################
+  # within-study variance
+
+  # between-study variance
+
+  # number of peaks
+
+  # number of clusters
+
+  # ditribution of cluster sizes
+
+  # distribution of peak heights
+
+
+################################################################################
+# Simulate meta-analysis set and write out results
+################################################################################
+  # Simulate
+
+  # t-maps
+
+  # z-maps
+
+  # ES-maps
+
+  # FSL output
+
+  # SPM output
+
+  # cluster and peak locations, size and height
+
+  # peak coordinates
+
+
+
+################################################################################
+#                               EXTRA                                          #
+################################################################################
+# Human Connectome Project
+  # 1. Is there a between-study variance?
+  # 2. How much do these differ from our parameters
+
+
