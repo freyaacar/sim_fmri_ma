7684751b65c8e579c45bc0d694f9f1f7cb7bf0f5
facar <freya.acar@gmail.com>
2017-04-13 08:31:24 +0200

    Initialize code for pooling hpc subjects

---
+++ Pool_subj_hpc.R
@@ -0,0 +1,8 @@
+library(oro.nifti)
+
+
+args = commandArgs(trailingOnly=TRUE)
+
+HPCfolder <- "/user/data/gent/gvo000/gvo00022/00_data/05_motor/"
+scratchdir <- "/user/scratch/gent/vsc414/vsc41410/Tempresultsn20/"
+resultsdir <- "/user/scratch/gent/vsc414/vsc41410/resultsn20/"
