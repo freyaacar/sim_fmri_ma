6562fe1a2053c133fc55b9d08ddd352b826a5a4a
facar <freya.acar@gmail.com>
2017-04-13 13:25:44 +0200

    Compute group level t-maps

--- Pool_subj_hpc.R
+++ Pool_subj_hpc.R
@@ -1,4 +1,5 @@
 library(oro.nifti)
+fslpath <- '/usr/local/fsl/bin/'
 
 ################################################################################
 # Determine parameters of real meta-analysis
@@ -22,18 +23,37 @@ library(oro.nifti)
 		copes <- readNIfTI("HCP/MOTOR_10_cope.nii.gz", verbose = FALSE, warn = -1, reorient = TRUE, call = NULL)
 		varcopes <- readNIfTI("HCP/MOTOR_10_varcope.nii.gz", verbose = FALSE, warn = -1, reorient = TRUE, call = NULL)
 
+	# construct mask
+		masks <- ifelse(copes*varcopes == 0, 0, 1)
+    	mask.hcp <- ifelse(apply(masks,c(1,2,3),mean) == 1, 1, 0)
+    	table(mask.hcp)
+    	setwd(home)
+    	masknifti <- nifti(img=mask.hcp, dim=DIM, datatype=2)
+		writeNIfTI(masknifti,filename = "HCP/mask",gzipped=FALSE)
+
 	# Write out cope files per study
 		i.temp <- 1
 		for (k in 1:k.hcp) {
 			cope.temp <- array(data=NA,dim=c(n.perst.hpc[k],DIM))
+			varcope.temp <- array(data=NA,dim=c(n.perst.hpc[k],DIM))
 			cope.temp <- copes[,,,i.temp:(i.temp+n.perst.hpc[k]-1)]
-			writeNIfTI(cope.temp, filename = paste("cope",k,sep=''),gzipped=FALSE)
+			varcope.temp <- varcopes[,,,i.temp:(i.temp+n.perst.hpc[k]-1)]
+			cope.temp.n <- nifti(img=cope.temp, dim=c(DIM,n.perst.hpc[k]), datatype=16)
+			varcope.temp.n <- nifti(img=varcope.temp, dim=c(DIM,n.perst.hpc[k]), datatype=16)
+			writeNIfTI(cope.temp.n, filename = paste("HCP/cope",k,sep=''),gzipped=FALSE)
+			writeNIfTI(varcope.temp.n, filename = paste("HCP/varcope",k,sep=''),gzipped=FALSE)
+			system(paste(fslpath,"fslcpgeom HCP/MOTOR_10_cope HCP/cope",k,sep=""))
+			system(paste(fslpath,"fslcpgeom HCP/MOTOR_10_cope HCP/varcope",k,sep=""))
 			i.temp <- i.temp + n.perst.hpc[k]
 		}
 
 ################################################################################
-# Perform 2nd level group analysis
+# Compute group level t-maps
 ################################################################################
+	for (k in 1:k.hcp) {
+		fsl_model_cmd = paste(fslpath,"randomise -i cope",k," -o output",k," -1 --glm_output",sep="")
+		system(fsl_model_cmd)
+	}
 
 
 ################################################################################
@@ -99,9 +119,72 @@ library(oro.nifti)
 	wsvar[1,50,50,50]
 
 ################################################################################
-# Fixend and random effects meta-analysis
+# Fixed and random effects meta-analysis
 ################################################################################
 
 
+
+
+
+
+
+
+
+
+
+################################################################################
+# Perform 2nd level group analysis - Credit to Han! - Later when we have time
+################################################################################
+	DataWrite <- "HCP/"
+	
+
+	for (k in 1:k.hcp) {
+		# Write auxiliarly files to DataWrite. We need:
+	    # GRCOPE in nifti
+	    # GRVARCOPE in nifti
+	    # 4D mask
+	    # design.mat file
+	    # design.grp file
+	    # design.con file
+
+	    #----- 1 ----#
+	    ### Design.mat
+	    fileCon <- paste(DataWrite,"/design.mat",sep="")
+	    # Text to be written to the file
+	    cat('/NumWaves\t1
+	    /NumPoints\t',paste(n.perst.hpc[k],sep=''),'
+	    /PPheights\t\t1.000000e+00
+	    /Matrix
+	    ',rep("1.000000e+00\n",n.perst.hpc[k]),file=fileCon)
+
+	    #----- 2 ----#
+	    ### Design.con
+	    fileCon <- file(paste(DataWrite,"/design.con", sep=""))
+	    	writeLines('/ContrastName1	Group Average
+	    /NumWaves	1
+	    /NumContrasts	1
+	    /PPheights		1.000000e+00
+	    /RequiredEffect		5.034
+	    /Matrix
+	    1.000000e+00
+	    ',fileCon)
+	    close(fileCon)
+
+	      #----- 3 ----#
+	      ### Design.grp
+	    fileCon <- paste(DataWrite,"/design.grp",sep="")
+	    # Text to be written to the file
+	    cat('/NumWaves\t1
+	    /NumPoints\t',paste(n.perst.hpc[k],sep=''),'
+	    /Matrix
+	    ',rep("1\n",n.perst.hpc[k]),file=fileCon)
+
+	    # FSL TIME!
+	    setwd(DataWrite)
+	    command <- paste(fslpath, 'flameo --cope=cope',k,' --vc=varcope',k,' --mask=mask --ld=study',k,'_stats --dm=design.mat --cs=design.grp --tc=design.con --runmode=flame1', sep='')
+	    Sys.setenv(FSLOUTPUTTYPE="NIFTI")
+	    system(command)
+	}
+	setwd(home)
 
 
